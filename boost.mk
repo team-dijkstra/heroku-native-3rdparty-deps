8bf7f4d7ac3b71f099d24594d2ec2551f25c58c9
Matt Voroney <matt.voroney@gmail.com>
2012-10-23 16:33:47 -0600

    added mpi support to boost build.

--- boost.mk
+++ boost.mk
@@ -4,9 +4,14 @@ LIB_URL := http://sourceforge.net/projects/boost/files/boost/1.51.0/boost_1_51_0
 LIB_VERSION := 1.51.0
 
 CONFIGURE = ./bootstrap.sh --prefix=$(INSTALLDIR)
-BUILD = @echo build deferred to install
+BUILD = ./b2 
 INSTALL = ./b2 install
 
 DEPENDENCIES := bzip2
+
+# add mpi support to the build
+config-extra: config
+	echo 'using mpi ;' >> user-config.jam
+build: config-extra
 
 -include $(HOME)/build/heroku.mk
