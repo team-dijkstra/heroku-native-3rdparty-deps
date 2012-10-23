3aa206f9ac1bb770a44097ce3432eb12027cc1aa
Matt Voroney <matt.voroney@gmail.com>
2012-10-22 23:00:19 -0600

    added log4cplus.mk to build a more supported logging framework for pion.

---
+++ log4cplus.mk
@@ -0,0 +1,9 @@
+
+LIBNAME := log4cplus
+LIB_URL := http://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.1.0/log4cplus-1.1.0.tar.xz
+LIB_VERSION := 1.1.0
+
+DEPENDENCIES :=
+
+-include $(HOME)/build/heroku.mk
+
